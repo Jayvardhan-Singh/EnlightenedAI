93d7053b250e9d415ed0b0b2d1e644604add1861
Jayvardhan Singh <jayvardhanmd@gmail.com>
2026-02-10 13:24:54 +0530

    Some text fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.header("EnlightenedAI")
 
-agent = st.radio("Select style of Response: ", ["EnlightenedAI 0.0:0b", "Non-Dual", "Neo-Advaita", "JK"])
+agent = st.radio("Select style of Response: ", ["EnlightenedAI", "Non-Dual", "Neo-Advaita", "JK"])
 
 
 st.slider("Temperature: ", min_value = 0.0, max_value = 2.0, value = 1.0)
@@ -10,10 +10,10 @@ st.selectbox("Thinking: ", ['On', 'Off'])
 
 question = st.text_area("Type your question: ")
 if st.button("Submit"):
-    if agent == "EnlightenedAI 0.0:0b":
+    if agent == "EnlightenedAI":
         st.write("")
     elif agent == "Non-Dual":
-        st.write("Who is the Submitter")
+        st.write("Introspect: Who is the Submitter?")
     elif agent == "Neo-Advaita":
         st.write("Disclaimer: There will be no spiritual progress with my answers.")
     elif agent == "JK":
