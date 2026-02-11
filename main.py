7ad7c44ccde84688be656bf8bdaad20d88aa63fc
Jayvardhan Singh <jayvardhanmd@gmail.com>
2026-02-11 09:43:55 +0530

    Add Max characters limits in the Text Area

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ agent = st.radio("Select style of Response: ", ["EnlightenedAI", "Non-Dual", "Ne
 st.slider("Temperature: ", min_value = 0.0, max_value = 2.0, value = 1.0)
 st.selectbox("Thinking: ", ['On', 'Off'])
 
-question = st.text_area("Type your question: ")
+MAX_CHARS = 100
+question = st.text_area("Type your question: ", max_chars=MAX_CHARS, placeholder=f"Maximum {MAX_CHARS} characters allowed")
 if st.button("Submit"):
     if agent == "EnlightenedAI":
         st.write("")
     elif agent == "Non-Dual":
         st.write("Introspect: Who is the Submitter?")
     elif agent == "Neo-Advaita":
-        st.write("Disclaimer: There will be no spiritual progress with my answers.")
+        st.write("Your spiritual progress will not be affected by my answers.")
     elif agent == "JK":
         st.write("Submitter is the submitted.")
