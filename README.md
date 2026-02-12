7e1f6c03feeec2a7cf3450ed0bd42ee250066786
Jayvardhan Singh <jayvardhanmd@gmail.com>
2026-02-12 10:12:49 +0530

    Add Streaming and Only EnlightenedAI Response

--- README.md
+++ README.md
@@ -4,5 +4,3 @@ This is a Fun Project.
 Author - Jayvardhan Singh
 <br>
 Enlightenment may be Everything or Nothing. Since training an AI with Everything is costly, this AI is trained on Nothing!
-<br>
-In addition to answers from Enlightened AI users can also choose Answers in style of NDA (Non-Dual Awareness), Neo-Advaita etc.
--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import streamlit as st
+import time
 
 st.header("EnlightenedAI")
-
-agent = st.radio("Select style of Response: ", ["EnlightenedAI", "Non-Dual", "Neo-Advaita", "JK"])
-
+st.subheader("Satisfy your spiritual quest")
 
 st.slider("Temperature: ", min_value = 0.0, max_value = 2.0, value = 1.0)
-st.selectbox("Thinking: ", ['On', 'Off'])
+thinking = st.selectbox("Thinking: ", ['On', 'Off'])
 
 MAX_CHARS = 100
 question = st.text_area("Type your question: ", max_chars=MAX_CHARS, placeholder=f"Maximum {MAX_CHARS} characters allowed")
+
+def stream_content():
+        full_text = f". . . thinking . . .     \nthe user has asked '{question}'... but the problem is that i am an EnlightenedAI . . .  . . .  . . . i am supposed to give the enlightened answer. . . .  \n  \n. . .  otherwise there is no point in asking me the question . . .  \n. . .  there are plenty of Non Enlightened AIs out there  \n ...done thinking."
+        for word in full_text.split(" "):
+                yield word + " "
+                time.sleep(0.15)
 if st.button("Submit"):
-    if agent == "EnlightenedAI":
-        st.write("")
-    elif agent == "Non-Dual":
-        st.write("Introspect: Who is the Submitter?")
-    elif agent == "Neo-Advaita":
-        st.write("Your spiritual progress will not be affected by my answers.")
-    elif agent == "JK":
-        st.write("Submitter is the submitted.")
+        if question and thinking == 'On':
+                 st.write(stream_content)
